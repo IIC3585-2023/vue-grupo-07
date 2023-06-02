ddbf0c5e7bccbf7ddd9709d3c9220d72c9615c4c
Benjamín Alamos <benjalamos@uc.cl>
2023-06-02 10:32:14 -0400

    initial services.js

---
+++ src/sevices.js
@@ -0,0 +1,84 @@
+import axios from "axios";
+
+const api_url = import.meta.env.VITE_API_URL
+
+export async function getAllCharacters(page) {
+  try {
+    const res = await axios.get(
+      `${api_url}/character?page=${page}`
+    );
+    const characters = res.data.results.map(character => {
+      const { id, name, image } = character;
+      return { id, name, image };
+    });
+    return characters
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getCharacterById(character_id) {
+  try {
+    const res = await axios.get(
+      `${api_url}/character/${character_id}`
+    );
+    const character = res.data
+    return character
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getAllEpisodes(page) {
+  try {
+    const res = await axios.get(
+      `${api_url}/episode?page=${page}`
+    );
+    const episodes = res.data.results.map(ep => {
+      const { id, name, episode, air_date } = ep;
+      return { id, name, episode, air_date };
+    });
+    return episodes
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getEpisodeById(episode_id) {
+  try {
+    const res = await axios.get(
+      `${api_url}/episode/${episode_id}`
+    );
+    const episode = res.data
+    return episode
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getAllLocations(page) {
+  try {
+    const res = await axios.get(
+      `${api_url}/location?page=${page}`
+    );
+    const locations = res.data.results.map(location => {
+      const { id, name, type, dimension } = location;
+      return { id, name, type, dimension };
+    });
+    return locations
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getLocationById(location_id) {
+  try {
+    const res = await axios.get(
+      `${api_url}/location/${location_id}`
+    );
+    const location = res.data
+    return location
+  } catch (error) {
+    console.log(error)
+  }
+}
---
+++ src/views/Episodes.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Episodes</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis magnam
+    aspernatur explicabo repellat hic enim in nisi doloremque illum recusandae,
+    eveniet alias sequi dolore assumenda nostrum. Tempora, nobis quaerat.
+    Adipisci?
+  </p>
+</template>
